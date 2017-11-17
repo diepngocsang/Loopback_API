7397f26516ce75a44b18de862bee20e55ac01ce5
dchau5 <dchau5@csc.com>
2017-11-17 11:15:48 +0700

    Update Game model

--- server/boot/create-model-default.js
+++ server/boot/create-model-default.js
@@ -1,4 +1,5 @@
 var async = require('async');
+var dataDefault = require('./create_data_default.js');
 
 module.exports = function (app) {
     //data sources
@@ -12,6 +13,7 @@ module.exports = function (app) {
     var Odd_Type = app.models.Odd_Type;
 
     var Team = app.models.Team;
+    var Game = app.models.Game;
 
     //create all models
     async.parallel({
@@ -19,7 +21,8 @@ module.exports = function (app) {
         admin: async.apply(createAdmin),
         odd_types: async.apply(createOddTypes),
         settings: async.apply(createSettings),
-        teams: async.apply(createTeams)
+        teams: async.apply(createTeams),
+        games: async.apply(createGames)
     }, function(err, results) {
         if (err) throw err;
 
@@ -121,398 +124,10 @@ module.exports = function (app) {
     }
 
     //create Teams
-    var arrTeam = [
-        {
-            teamCode: "RUS", 
-            teamName: "RUSSIA", 
-            picture: "uploads/images/russia.png", 
-            group: "", 
-            players: [],
-            win: "",
-            lose: "",
-            draw: "",
-            goalsAgainst: "",
-            goalsDifference: "",
-            currentRound: "" 
-        },{
-            teamCode: "BRA", 
-            teamName: "BRAZIL", 
-            picture: "uploads/images/brazil.png", 
-            group: "", 
-            players: [],
-            win: "",
-            lose: "",
-            draw: "",
-            goalsAgainst: "",
-            goalsDifference: "",
-            currentRound: "" 
-        },{
-            teamCode: "IRN", 
-            teamName: "IRAN", 
-            picture: "uploads/images/iran.png", 
-            group: "", 
-            players: [],
-            win: "",
-            lose: "",
-            draw: "",
-            goalsAgainst: "",
-            goalsDifference: "",
-            currentRound: "" 
-        },{
-            teamCode: "JPN", 
-            teamName: "JAPAN", 
-            picture: "uploads/images/japan.png", 
-            group: "", 
-            players: [],
-            win: "",
-            lose: "",
-            draw: "",
-            goalsAgainst: "",
-            goalsDifference: "",
-            currentRound: "" 
-        },{
-            teamCode: "MEX", 
-            teamName: "MEXICO", 
-            picture: "uploads/images/mexico.png", 
-            group: "", 
-            players: [],
-            win: "",
-            lose: "",
-            draw: "",
-            goalsAgainst: "",
-            goalsDifference: "",
-            currentRound: "" 
-        },{
-            teamCode: "BEL", 
-            teamName: "BELGIUM", 
-            picture: "uploads/images/belgium.png", 
-            group: "", 
-            players: [],
-            win: "",
-            lose: "",
-            draw: "",
-            goalsAgainst: "",
-            goalsDifference: "",
-            currentRound: "" 
-        },{
-            teamCode: "KOR", 
-            teamName: "SOUTH KOREA", 
-            picture: "uploads/images/south_korea.png", 
-            group: "", 
-            players: [],
-            win: "",
-            lose: "",
-            draw: "",
-            goalsAgainst: "",
-            goalsDifference: "",
-            currentRound: "" 
-        },{
-            teamCode: "KSA", 
-            teamName: "SAUDI ARABIA", 
-            picture: "uploads/images/saudi_arabia.png", 
-            group: "", 
-            players: [],
-            win: "",
-            lose: "",
-            draw: "",
-            goalsAgainst: "",
-            goalsDifference: "",
-            currentRound: "" 
-        },{
-            teamCode: "GER", 
-            teamName: "GERMANY", 
-            picture: "uploads/images/germany.png", 
-            group: "", 
-            players: [],
-            win: "",
-            lose: "",
-            draw: "",
-            goalsAgainst: "",
-            goalsDifference: "",
-            currentRound: "" 
-        },{
-            teamCode: "ENG", 
-            teamName: "ENGLAND", 
-            picture: "uploads/images/england.png", 
-            group: "", 
-            players: [],
-            win: "",
-            lose: "",
-            draw: "",
-            goalsAgainst: "",
-            goalsDifference: "",
-            currentRound: "" 
-        },{
-            teamCode: "ESP", 
-            teamName: "SPAIN", 
-            picture: "uploads/images/spain.png", 
-            group: "", 
-            players: [],
-            win: "",
-            lose: "",
-            draw: "",
-            goalsAgainst: "",
-            goalsDifference: "",
-            currentRound: "" 
-        },{
-            teamCode: "NGA", 
-            teamName: "NIGERIA", 
-            picture: "uploads/images/nigeria.png", 
-            group: "", 
-            players: [],
-            win: "",
-            lose: "",
-            draw: "",
-            goalsAgainst: "",
-            goalsDifference: "",
-            currentRound: "" 
-        },{
-            teamCode: "CRC", 
-            teamName: "COSTA RICA", 
-            picture: "uploads/images/costa_rica.png", 
-            group: "", 
-            players: [],
-            win: "",
-            lose: "",
-            draw: "",
-            goalsAgainst: "",
-            goalsDifference: "",
-            currentRound: "" 
-        },{
-            teamCode: "POL", 
-            teamName: "POLAND", 
-            picture: "uploads/images/poland.png", 
-            group: "", 
-            players: [],
-            win: "",
-            lose: "",
-            draw: "",
-            goalsAgainst: "",
-            goalsDifference: "",
-            currentRound: "" 
-        },{
-            teamCode: "EGY", 
-            teamName: "EGYPT", 
-            picture: "uploads/images/egypt.png", 
-            group: "", 
-            players: [],
-            win: "",
-            lose: "",
-            draw: "",
-            goalsAgainst: "",
-            goalsDifference: "",
-            currentRound: "" 
-        },{
-            teamCode: "ISL", 
-            teamName: "ICELAND", 
-            picture: "uploads/images/iceland.png", 
-            group: "", 
-            players: [],
-            win: "",
-            lose: "",
-            draw: "",
-            goalsAgainst: "",
-            goalsDifference: "",
-            currentRound: "" 
-        },{
-            teamCode: "SRB", 
-            teamName: "SERBIA", 
-            picture: "uploads/images/serbia.png", 
-            group: "", 
-            players: [],
-            win: "",
-            lose: "",
-            draw: "",
-            goalsAgainst: "",
-            goalsDifference: "",
-            currentRound: "" 
-        },{
-            teamCode: "POR", 
-            teamName: "PORTUGAL", 
-            picture: "uploads/images/portugal.png", 
-            group: "", 
-            players: [],
-            win: "",
-            lose: "",
-            draw: "",
-            goalsAgainst: "",
-            goalsDifference: "",
-            currentRound: "" 
-        },{
-            teamCode: "FRA", 
-            teamName: "FRANCE", 
-            picture: "uploads/images/france.png", 
-            group: "", 
-            players: [],
-            win: "",
-            lose: "",
-            draw: "",
-            goalsAgainst: "",
-            goalsDifference: "",
-            currentRound: "" 
-        },{
-            teamCode: "URU", 
-            teamName: "URUGUAY", 
-            picture: "uploads/images/uruguay.png", 
-            group: "", 
-            players: [],
-            win: "",
-            lose: "",
-            draw: "",
-            goalsAgainst: "",
-            goalsDifference: "",
-            currentRound: "" 
-        },{
-            teamCode: "ARG", 
-            teamName: "ARGENTINA", 
-            picture: "uploads/images/argentina.png", 
-            group: "", 
-            players: [],
-            win: "",
-            lose: "",
-            draw: "",
-            goalsAgainst: "",
-            goalsDifference: "",
-            currentRound: "" 
-        },{
-            teamCode: "COL", 
-            teamName: "COLOMBIA", 
-            picture: "uploads/images/colombia.png", 
-            group: "", 
-            players: [],
-            win: "",
-            lose: "",
-            draw: "",
-            goalsAgainst: "",
-            goalsDifference: "",
-            currentRound: "" 
-        },{
-            teamCode: "PAN", 
-            teamName: "PANAMA", 
-            picture: "uploads/images/panama.png", 
-            group: "", 
-            players: [],
-            win: "",
-            lose: "",
-            draw: "",
-            goalsAgainst: "",
-            goalsDifference: "",
-            currentRound: "" 
-        },{
-            teamCode: "SEN", 
-            teamName: "SENEGAL", 
-            picture: "uploads/images/senegal.png", 
-            group: "", 
-            players: [],
-            win: "",
-            lose: "",
-            draw: "",
-            goalsAgainst: "",
-            goalsDifference: "",
-            currentRound: "" 
-        },{
-            teamCode: "MAR", 
-            teamName: "MOROCCO", 
-            picture: "uploads/images/morocco.png", 
-            group: "", 
-            players: [],
-            win: "",
-            lose: "",
-            draw: "",
-            goalsAgainst: "",
-            goalsDifference: "",
-            currentRound: "" 
-        },{
-            teamCode: "TUN", 
-            teamName: "TUNISIA", 
-            picture: "uploads/images/tunisia.png", 
-            group: "", 
-            players: [],
-            win: "",
-            lose: "",
-            draw: "",
-            goalsAgainst: "",
-            goalsDifference: "",
-            currentRound: "" 
-        },{
-            teamCode: "SUI", 
-            teamName: "SWITZERLAND", 
-            picture: "uploads/images/switzerland.png", 
-            group: "", 
-            players: [],
-            win: "",
-            lose: "",
-            draw: "",
-            goalsAgainst: "",
-            goalsDifference: "",
-            currentRound: "" 
-        },{
-            teamCode: "CRO", 
-            teamName: "CROATIA", 
-            picture: "uploads/images/croatia.png", 
-            group: "", 
-            players: [],
-            win: "",
-            lose: "",
-            draw: "",
-            goalsAgainst: "",
-            goalsDifference: "",
-            currentRound: "" 
-        },{
-            teamCode: "SWE", 
-            teamName: "SWEDEN", 
-            picture: "uploads/images/sweden.png", 
-            group: "", 
-            players: [],
-            win: "",
-            lose: "",
-            draw: "",
-            goalsAgainst: "",
-            goalsDifference: "",
-            currentRound: "" 
-        },{
-            teamCode: "DEN", 
-            teamName: "DENMARK", 
-            picture: "uploads/images/dermark.png", 
-            group: "", 
-            players: [],
-            win: "",
-            lose: "",
-            draw: "",
-            goalsAgainst: "",
-            goalsDifference: "",
-            currentRound: "" 
-        },{
-            teamCode: "AUS", 
-            teamName: "AUSTRALIA", 
-            picture: "uploads/images/australia.png", 
-            group: "", 
-            players: [],
-            win: "",
-            lose: "",
-            draw: "",
-            goalsAgainst: "",
-            goalsDifference: "",
-            currentRound: "" 
-        },{
-            teamCode: "PER", 
-            teamName: "PERU", 
-            picture: "uploads/images/peru.png", 
-            group: "", 
-            players: [],
-            win: "",
-            lose: "",
-            draw: "",
-            goalsAgainst: "",
-            goalsDifference: "",
-            currentRound: "" 
-        }
-    ];
-
     function createTeams(cb){
         Team.count(function(err, count){
             if(err==null&&count==0){
-                Team.create( arrTeam, function(err, teams){
+                Team.create( dataDefault.getArrTeam(), function(err, teams){
                     if (err) throw err;
         
                     console.log('Created teams:', teams);
@@ -521,5 +136,16 @@ module.exports = function (app) {
         });
     }
 
-
+    //create Teams
+    function createGames(cb){
+        Game.count(function(err, count){
+            if(err==null&&count==0){
+                Game.create( dataDefault.getArrGame(), function(err, games){
+                    if (err) throw err;
+        
+                    console.log('Created games:', games);
+                });
+            }
+        });
+    }
 };
---
+++ server/boot/create_data_default.js
@@ -0,0 +1,429 @@
+'use strict';
+
+module.exports = {
+    getArrTeam: function(){
+        return [
+            {
+                teamCode: "RUS", 
+                teamName: "RUSSIA", 
+                picture: "uploads/images/russia.png", 
+                group: "A", 
+                players: [],
+                win: "",
+                lose: "",
+                draw: "",
+                goalsAgainst: "",
+                goalsDifference: "",
+                currentRound: "" 
+            },{
+                teamCode: "BRA", 
+                teamName: "BRAZIL", 
+                picture: "uploads/images/brazil.png", 
+                group: "A", 
+                players: [],
+                win: "",
+                lose: "",
+                draw: "",
+                goalsAgainst: "",
+                goalsDifference: "",
+                currentRound: "" 
+            },{
+                teamCode: "IRN", 
+                teamName: "IRAN", 
+                picture: "uploads/images/iran.png", 
+                group: "A", 
+                players: [],
+                win: "",
+                lose: "",
+                draw: "",
+                goalsAgainst: "",
+                goalsDifference: "",
+                currentRound: "" 
+            },{
+                teamCode: "JPN", 
+                teamName: "JAPAN", 
+                picture: "uploads/images/japan.png", 
+                group: "A", 
+                players: [],
+                win: "",
+                lose: "",
+                draw: "",
+                goalsAgainst: "",
+                goalsDifference: "",
+                currentRound: "" 
+            },{
+                teamCode: "MEX", 
+                teamName: "MEXICO", 
+                picture: "uploads/images/mexico.png", 
+                group: "B", 
+                players: [],
+                win: "",
+                lose: "",
+                draw: "",
+                goalsAgainst: "",
+                goalsDifference: "",
+                currentRound: "" 
+            },{
+                teamCode: "BEL", 
+                teamName: "BELGIUM", 
+                picture: "uploads/images/belgium.png", 
+                group: "B", 
+                players: [],
+                win: "",
+                lose: "",
+                draw: "",
+                goalsAgainst: "",
+                goalsDifference: "",
+                currentRound: "" 
+            },{
+                teamCode: "KOR", 
+                teamName: "SOUTH KOREA", 
+                picture: "uploads/images/south_korea.png", 
+                group: "B", 
+                players: [],
+                win: "",
+                lose: "",
+                draw: "",
+                goalsAgainst: "",
+                goalsDifference: "",
+                currentRound: "" 
+            },{
+                teamCode: "KSA", 
+                teamName: "SAUDI ARABIA", 
+                picture: "uploads/images/saudi_arabia.png", 
+                group: "B", 
+                players: [],
+                win: "",
+                lose: "",
+                draw: "",
+                goalsAgainst: "",
+                goalsDifference: "",
+                currentRound: "" 
+            },{
+                teamCode: "GER", 
+                teamName: "GERMANY", 
+                picture: "uploads/images/germany.png", 
+                group: "C", 
+                players: [],
+                win: "",
+                lose: "",
+                draw: "",
+                goalsAgainst: "",
+                goalsDifference: "",
+                currentRound: "" 
+            },{
+                teamCode: "ENG", 
+                teamName: "ENGLAND", 
+                picture: "uploads/images/england.png", 
+                group: "C", 
+                players: [],
+                win: "",
+                lose: "",
+                draw: "",
+                goalsAgainst: "",
+                goalsDifference: "",
+                currentRound: "" 
+            },{
+                teamCode: "ESP", 
+                teamName: "SPAIN", 
+                picture: "uploads/images/spain.png", 
+                group: "C", 
+                players: [],
+                win: "",
+                lose: "",
+                draw: "",
+                goalsAgainst: "",
+                goalsDifference: "",
+                currentRound: "" 
+            },{
+                teamCode: "NGA", 
+                teamName: "NIGERIA", 
+                picture: "uploads/images/nigeria.png", 
+                group: "C", 
+                players: [],
+                win: "",
+                lose: "",
+                draw: "",
+                goalsAgainst: "",
+                goalsDifference: "",
+                currentRound: "" 
+            },{
+                teamCode: "CRC", 
+                teamName: "COSTA RICA", 
+                picture: "uploads/images/costa_rica.png", 
+                group: "D", 
+                players: [],
+                win: "",
+                lose: "",
+                draw: "",
+                goalsAgainst: "",
+                goalsDifference: "",
+                currentRound: "" 
+            },{
+                teamCode: "POL", 
+                teamName: "POLAND", 
+                picture: "uploads/images/poland.png", 
+                group: "D", 
+                players: [],
+                win: "",
+                lose: "",
+                draw: "",
+                goalsAgainst: "",
+                goalsDifference: "",
+                currentRound: "" 
+            },{
+                teamCode: "EGY", 
+                teamName: "EGYPT", 
+                picture: "uploads/images/egypt.png", 
+                group: "D", 
+                players: [],
+                win: "",
+                lose: "",
+                draw: "",
+                goalsAgainst: "",
+                goalsDifference: "",
+                currentRound: "" 
+            },{
+                teamCode: "ISL", 
+                teamName: "ICELAND", 
+                picture: "uploads/images/iceland.png", 
+                group: "D", 
+                players: [],
+                win: "",
+                lose: "",
+                draw: "",
+                goalsAgainst: "",
+                goalsDifference: "",
+                currentRound: "" 
+            },{
+                teamCode: "SRB", 
+                teamName: "SERBIA", 
+                picture: "uploads/images/serbia.png", 
+                group: "E", 
+                players: [],
+                win: "",
+                lose: "",
+                draw: "",
+                goalsAgainst: "",
+                goalsDifference: "",
+                currentRound: "" 
+            },{
+                teamCode: "POR", 
+                teamName: "PORTUGAL", 
+                picture: "uploads/images/portugal.png", 
+                group: "E", 
+                players: [],
+                win: "",
+                lose: "",
+                draw: "",
+                goalsAgainst: "",
+                goalsDifference: "",
+                currentRound: "" 
+            },{
+                teamCode: "FRA", 
+                teamName: "FRANCE", 
+                picture: "uploads/images/france.png", 
+                group: "E", 
+                players: [],
+                win: "",
+                lose: "",
+                draw: "",
+                goalsAgainst: "",
+                goalsDifference: "",
+                currentRound: "" 
+            },{
+                teamCode: "URU", 
+                teamName: "URUGUAY", 
+                picture: "uploads/images/uruguay.png", 
+                group: "E", 
+                players: [],
+                win: "",
+                lose: "",
+                draw: "",
+                goalsAgainst: "",
+                goalsDifference: "",
+                currentRound: "" 
+            },{
+                teamCode: "ARG", 
+                teamName: "ARGENTINA", 
+                picture: "uploads/images/argentina.png", 
+                group: "F", 
+                players: [],
+                win: "",
+                lose: "",
+                draw: "",
+                goalsAgainst: "",
+                goalsDifference: "",
+                currentRound: "" 
+            },{
+                teamCode: "COL", 
+                teamName: "COLOMBIA", 
+                picture: "uploads/images/colombia.png", 
+                group: "F", 
+                players: [],
+                win: "",
+                lose: "",
+                draw: "",
+                goalsAgainst: "",
+                goalsDifference: "",
+                currentRound: "" 
+            },{
+                teamCode: "PAN", 
+                teamName: "PANAMA", 
+                picture: "uploads/images/panama.png", 
+                group: "F", 
+                players: [],
+                win: "",
+                lose: "",
+                draw: "",
+                goalsAgainst: "",
+                goalsDifference: "",
+                currentRound: "" 
+            },{
+                teamCode: "SEN", 
+                teamName: "SENEGAL", 
+                picture: "uploads/images/senegal.png", 
+                group: "F", 
+                players: [],
+                win: "",
+                lose: "",
+                draw: "",
+                goalsAgainst: "",
+                goalsDifference: "",
+                currentRound: "" 
+            },{
+                teamCode: "MAR", 
+                teamName: "MOROCCO", 
+                picture: "uploads/images/morocco.png", 
+                group: "G", 
+                players: [],
+                win: "",
+                lose: "",
+                draw: "",
+                goalsAgainst: "",
+                goalsDifference: "",
+                currentRound: "" 
+            },{
+                teamCode: "TUN", 
+                teamName: "TUNISIA", 
+                picture: "uploads/images/tunisia.png", 
+                group: "G", 
+                players: [],
+                win: "",
+                lose: "",
+                draw: "",
+                goalsAgainst: "",
+                goalsDifference: "",
+                currentRound: "" 
+            },{
+                teamCode: "SUI", 
+                teamName: "SWITZERLAND", 
+                picture: "uploads/images/switzerland.png", 
+                group: "G", 
+                players: [],
+                win: "",
+                lose: "",
+                draw: "",
+                goalsAgainst: "",
+                goalsDifference: "",
+                currentRound: "" 
+            },{
+                teamCode: "CRO", 
+                teamName: "CROATIA", 
+                picture: "uploads/images/croatia.png", 
+                group: "G", 
+                players: [],
+                win: "",
+                lose: "",
+                draw: "",
+                goalsAgainst: "",
+                goalsDifference: "",
+                currentRound: "" 
+            },{
+                teamCode: "SWE", 
+                teamName: "SWEDEN", 
+                picture: "uploads/images/sweden.png", 
+                group: "H", 
+                players: [],
+                win: "",
+                lose: "",
+                draw: "",
+                goalsAgainst: "",
+                goalsDifference: "",
+                currentRound: "" 
+            },{
+                teamCode: "DEN", 
+                teamName: "DENMARK", 
+                picture: "uploads/images/dermark.png", 
+                group: "H", 
+                players: [],
+                win: "",
+                lose: "",
+                draw: "",
+                goalsAgainst: "",
+                goalsDifference: "",
+                currentRound: "" 
+            },{
+                teamCode: "AUS", 
+                teamName: "AUSTRALIA", 
+                picture: "uploads/images/australia.png", 
+                group: "H", 
+                players: [],
+                win: "",
+                lose: "",
+                draw: "",
+                goalsAgainst: "",
+                goalsDifference: "",
+                currentRound: "" 
+            },{
+                teamCode: "PER", 
+                teamName: "PERU", 
+                picture: "uploads/images/peru.png", 
+                group: "H", 
+                players: [],
+                win: "",
+                lose: "",
+                draw: "",
+                goalsAgainst: "",
+                goalsDifference: "",
+                currentRound: "" 
+            }
+        ];
+    },
+    getArrGame : function (){
+        return [
+            {
+                "gameId": "RUSBRA_20180101",
+                "teamHome": "RUS",
+                "teamAway": "BRA",
+                "odds": [
+                    {"key":"Home","rate":1.7},
+                    {"key":"Away","rate":1},
+                    {"key":"Draw","rate":1.2},
+                    {"key":"Over","rate":2},
+                    {"key":"Under","rate":1}
+                ],
+                "isActive": false,
+                "result": {},
+                "date": "2018-01-01T07:00:00Z",
+                "stadium": "Stadium 1"
+            },
+            {
+                "gameId": "IRNJPN_20180101",
+                "teamHome": "IRN",
+                "teamAway": "JPN",
+                "odds": [
+                    {"key":"Home","rate":1.7},
+                    {"key":"Away","rate":1},
+                    {"key":"Draw","rate":1.2},
+                    {"key":"Over","rate":2},
+                    {"key":"Under","rate":1}
+                ],
+                "isActive": false,
+                "result": {},
+                "date": "2018-01-01T07:00:00Z",
+                "stadium": "Stadium 2"
+            }
+        ]
+    }
+};
