71aafdae41bfe29e60f2b44e6a192877d49b3b83
dchau5 <dchau5@csc.com>
2017-11-23 15:59:24 +0700

    Add function get game with team home and away information

--- common/models/game.js
+++ common/models/game.js
@@ -1,5 +1,35 @@
 'use strict';
+var app = require('../../server/server');
 
 module.exports = function(Game) {
+    Game.remoteMethod(
+        'getGameWithTeam',
+        {
+            description: "Allows get games with information of team home and team away",
+            http: { path: '/getGameWithTeam', verb: 'get' },
+            accepts: [
+                { arg: 'ctx', type: 'object', http: { source: 'context' } }
+            ]
+        }
+    );
+    Game.getGameWithTeam = function (ctx, cb) {
+        var baseUrl = app.get('url').replace(/\/$/, '');
 
+        try {
+            Game.find({include: ['teamHome', 'teamAway']}, function(err, response) {
+                if (err) {
+                    return next(err);
+                }
+                else{
+                    ctx.res.status(200).json({
+                        success: true,
+                        message: "",
+                        data: response
+                    });
+                }
+            });
+        } catch (err) {
+            cb(err);
+        }
+    };
 };
